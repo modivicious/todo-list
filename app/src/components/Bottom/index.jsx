aba15face47ad5285bf2dd0bea2cebe4a1c6f116
Daniele <prihood@bk.ru>
2022-04-11 04:39:19 +0300

    add save info in bottom

--- app/src/components/Bottom/index.jsx
+++ app/src/components/Bottom/index.jsx
@@ -4,19 +4,24 @@ import styles from "./Bottom.module.scss";
 
 const Bottom = ({ onAdd, onClear }) => {
   return (
-    <div className={styles.bottom}>
-      <button
-        className={`${styles.button} ${styles.add}`}
-        onClick={() => onAdd()}
-      >
-        Add new task
-      </button>
-      <button
-        className={`${styles.button} ${styles.clear}`}
-        onClick={() => onClear()}
-      >
-        Clear all
-      </button>
+    <div>
+      <div className={styles.buttonsBox}>
+        <button
+          className={`${styles.button} ${styles.add}`}
+          onClick={() => onAdd()}
+        >
+          Add new task
+        </button>
+        <button
+          className={`${styles.button} ${styles.clear}`}
+          onClick={() => onClear()}
+        >
+          Clear all
+        </button>
+      </div>
+      <div className={styles.saveInfo}>
+        <span>You should unfocus the input fields to save your changes.</span>
+      </div>
     </div>
   );
 };
